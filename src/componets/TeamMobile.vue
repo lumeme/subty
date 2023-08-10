b17b54f8472d97df742df4b3ed05109f2ae3caf8
MelaJanni <melajannielli@gmail.com>
2023-08-10 12:14:40 -0300

    Fix: contact and pricing

--- src/componets/TeamMobile.vue
+++ src/componets/TeamMobile.vue
@@ -10,7 +10,7 @@
                     <div class="col-12 d-flex justify-content-center justify-content-lg-end align-items-center px-0 fix">
                         <div class="row justify-content-center">
                             <div class="col-12 d-flex flex-column justify-content-center align-items-center">
-                                <img src="../../img/team-icons/creator-1.svg" alt="">
+                                <img src="../../img/team-icons/nana.png" alt="" class="img-fix">
                                 <p class="p-creator">{{ $t('message.ceo') }}</p>
                                 <h2 class="name-creator">Naomi Merlo Batista</h2>
                             </div>
@@ -27,12 +27,12 @@
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-center align-items-lg-end px-0 padding d-none d-lg-flex">
+            <div class="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-center align-items-lg-end px-0 padding">
                 <div class="row justify-content-center w-100">
                     <div class="col-12 d-flex justify-content-center justify-content-lg-start align-items-center px-0 fix">
                         <div class="row justify-content-center">
                             <div class="col-12 d-flex flex-column justify-content-center align-items-center">
-                                <img src="../../img/team-icons/creator-2.svg" alt="">
+                                <img src="../../img/team-icons/lumeme.jpeg.jpg" alt="" class="img-fix">
                                 <p class="p-creator">{{ $t('message.ceo') }}</p>
                                 <h2 class="name-creator">Lucia Iglesias</h2>
                             </div>
