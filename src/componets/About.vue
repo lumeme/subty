ce4da34f2e7e134588dca417ba79ed48da17f131
Julius <juliodemartini11@gmail.com>
2023-09-08 10:21:29 -0300

    fix: about text

--- src/componets/About.vue
+++ src/componets/About.vue
@@ -1,13 +1,7 @@
 <template>
     <div class="col-12 col-lg-9 d-flex justify-content-center align-items-start section about padding-lg" id="about">
         <div class="row justify-content-center">
-            <div class="col-12 d-flex flex-column justify-content-center align-items-start">
-                <h2>{{ $t('message.about') }}</h2>
-            </div>
             <div class="col-12 col-lg-6 d-flex flex-column justify-content-center justify-content-lg-start align-items-start ">
-                <p>
-                    {{ $t('message.aboutSectionOne') }} 
-                </p>
                 <p class="pt-0">
                     {{ $t('message.aboutSectionTwo') }}<br>
                 </p>
@@ -22,14 +16,7 @@
                 </p>
                 <p class="grey">
                     {{ $t('message.aboutSectionSix') }}
-                </p>
-                <p class="grey">
-                    {{ $t('message.aboutSectionSeven') }}
-                </p>
-                <p class="grey">
-                    {{ $t('message.aboutSectionEight') }}
-                </p>
-                
+                </p>                
             </div>
             <div class="col-12 col-lg-6 d-flex justify-content-center justify-content-lg-end  align-items-center" >
                 <Astronaut/>
